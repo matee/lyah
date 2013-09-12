ac7a6c4cca612af2a0a6c7448d7a692b8d79f89d
Matus Laslofi <matus.laslofi@gmail.com>
2013-09-13 00:49:00 +0200

    I/O examples

---
+++ hello/shortlines.hs
@@ -0,0 +1,10 @@
+main = interact shortLinesOnly  
+  
+shortLinesOnly :: String -> String  
+shortLinesOnly input =   
+    let allLines = lines input  
+        shortLines = filter (\line -> length line < 10) allLines  
+        result = unlines shortLines  
+    in  result  
+    
+-- main = interact $ unlines . filter ((<10) . length) . lines      
